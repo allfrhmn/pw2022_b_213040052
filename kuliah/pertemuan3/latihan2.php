5e939fe707d95765a249b11b7ae6978582a0941b
= <=>
2022-05-23 13:13:16 +0700

    menyelesaikan latihan di pertemuan 3

---
+++ kuliah/pertemuan3/latihan2.php
@@ -0,0 +1,16 @@
+<?php 
+// Pengkondisian
+// if else
+// if else if else
+// ternary
+// switch
+
+$x = 30;
+if ($x < 20) {
+    echo "BENAR";
+} else if ($x == 20) {
+    echo "BINGO!";
+} else {
+    echo "SALAH";
+}
+?>
